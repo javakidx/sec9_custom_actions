1694edb5d71e99db048298d728b0ef3e3cd87f03
Bio Yang <javakidx@gmail.com>
2025-04-22 09:39:27 +0800

    Added deployment to AWS S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,6 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
+  // 1 Get the inputs
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2 Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
+
   core.notice('Hello from your custom JavaScript action!');
 }
 
